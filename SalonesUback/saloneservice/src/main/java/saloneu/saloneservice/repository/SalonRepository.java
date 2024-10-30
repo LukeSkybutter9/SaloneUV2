99a10d9b1aa8eacc97a9a8662f3faf255b55cd15
Fabian Escalante <fali.escalante@gmail.com>
2024-10-30 16:57:16 -0500

    DropSaloneuBack

--- SalonesUback/saloneservice/src/main/java/saloneu/saloneservice/repository/SalonRepository.java
+++
@@ -1,7 +0,0 @@
-package saloneu.saloneservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import saloneu.saloneservice.entity.Salon;
-
-public interface SalonRepository extends JpaRepository<Salon, Long> {
-}
