b8bdc404f165669fecfd8842a695305bb624a86c
Fabian Escalante <fali.escalante@gmail.com>
2024-11-20 22:59:25 -0500

    Commit from another side

--- saloneuproject/src/components/vue/ModalReserva.vue
+++ saloneuproject/src/components/vue/ModalReserva.vue
@@ -44,8 +44,6 @@
     const hoy = new Date();
     
     const año = hoy.getFullYear();
-    const mes = String(hoy.getMonth() + 1).padStart(2, '0');  // Mes en formato 2 dígitos
-    const dia = String(hoy.getDate()).padStart(2, '0');  // Día en formato 2 dígitos
     const mes = String(hoy.getMonth() + 1).padStart(2, '0'); 
     const dia = String(hoy.getDate()).padStart(2, '0'); 
     
